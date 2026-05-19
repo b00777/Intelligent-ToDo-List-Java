a5f85e5cf45b6894d544f1b27b3175c02b02899d
b00777 <86436928+b00777@users.noreply.github.com>
2026-05-19 21:19:11 +0530

    Added Task.java

---
+++ src/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+
+    String title;
+    int priority;
+
+    public Task(String title, int priority) {
+        this.title = title;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
