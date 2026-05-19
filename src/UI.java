42c70bcb5566eb565e2c085170311410aedf604f
b00777 <86436928+b00777@users.noreply.github.com>
2026-05-19 21:22:27 +0530

    Added UI.java

---
+++ src/UI.java
@@ -0,0 +1,12 @@
+public class UI {
+
+    public void displayWelcomeMessage() {
+
+        System.out.println("Welcome to Intelligent To-Do List");
+    }
+
+    public void displayTaskAdded() {
+
+        System.out.println("Task added successfully.");
+    }
+}
